54052429e4223b60bd7716883d31618bb08d21ca
Oleksandr Veselov <alexander.veselov.dev@gmail.com>
2024-09-02 02:16:48 +0300

    Add FPS test

--- examples/test.py
+++ examples/test.py
@@ -9,13 +9,6 @@ def get_driver(angle=90, mirrored=False):
     spi.max_speed_hz = 48000000
     return ILI9486(dc=24, rst=25, spi=spi, angle=angle, mirrored=mirrored)
 
-def test_gradient():
-    driver = get_driver()
-    image = create_gradient_image(driver.get_size())
-    driver.display(image)
-    time.sleep(10)
-    driver.reset()
-
 def test_rotations():
     for angle in [0, 90, 180, 270]:
         for mirrored in [False, True]:
@@ -25,8 +18,28 @@ def test_rotations():
             time.sleep(3)
             driver.reset()
 
+def test_fps():
+    driver = get_driver()
+    image = create_gradient_image(driver.get_size())
+    frames = 0
+    fps_data = []
+    frame_start_time = time.time()
+    while len(fps_data) < 10:
+        driver.display(image)
+        frames += 1
+        current_time = time.time()
+        elapsed_time = current_time - frame_start_time
+        if elapsed_time >= 1.0:
+            fps = frames / elapsed_time
+            fps_data.append(fps)
+            frame_start_time = current_time
+            frames = 0
+    driver.reset()
+    print("Min FPS:", min(fps_data))
+    print("Average FPS:", sum(fps_data) / len(fps_data))
+
 def main():
-    test_gradient()
+    test_fps()
     test_rotations()
 
 if __name__ == '__main__':
--- examples/utils.py
+++ examples/utils.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 
 def create_gradient_image(size):
-    gradient_image = Image.new('RGB', size)
+    gradient_image = Image.new("RGB", size)
     width, height = size
     for x in range(width):
         for y in range(height):
@@ -17,8 +17,8 @@ def create_text_image(size, rotation=0, mirrored=False):
     width, height = size
     config_text = f"Size: {width}x{height}, Angle: {rotation}, Mirrored: {mirrored}"
     static_text = "---> right"
-    image = Image.new('RGB', (width, height), color='white')
-    main_text_image = Image.new('RGBA', (width, height), (255, 255, 255, 0))
+    image = Image.new("RGB", (width, height), color="white")
+    main_text_image = Image.new("RGBA", (width, height), (255, 255, 255, 0))
     draw = ImageDraw.Draw(main_text_image)
     font = ImageFont.load_default()
     bbox = draw.textbbox((0, 0), config_text, font=font)
@@ -26,16 +26,16 @@ def create_text_image(size, rotation=0, mirrored=False):
     text_height = bbox[3] - bbox[1]
     text_x = (width - text_width) // 2
     text_y = (height - text_height) // 2
-    draw.text((text_x, text_y), config_text, fill='black', font=font)
+    draw.text((text_x, text_y), config_text, fill="black", font=font)
     if mirrored: main_text_image = main_text_image.transpose(Image.FLIP_LEFT_RIGHT)
     image.paste(main_text_image, (0, 0), main_text_image)
     arrow_font = ImageFont.load_default(20)
-    static_text_image = Image.new('RGBA', (width, height), (255, 255, 255, 0))
+    static_text_image = Image.new("RGBA", (width, height), (255, 255, 255, 0))
     static_draw = ImageDraw.Draw(static_text_image)
     static_bbox = static_draw.textbbox((0, 0), static_text, font=arrow_font)
     static_text_width = static_bbox[2] - static_bbox[0]
     static_text_x = (width - static_text_width) // 2
     static_text_y = text_y + text_height + 10
-    static_draw.text((static_text_x, static_text_y), static_text, fill='black', font=arrow_font)
+    static_draw.text((static_text_x, static_text_y), static_text, fill="black", font=arrow_font)
     image.paste(static_text_image, (0, 0), static_text_image)
     return image
